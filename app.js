125e76fb5dcdc5c1c399d888571315f2669c21ed
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2025-08-26 16:58:29 -0300

    Termino do Site. Criação do App

---
+++ app.js
@@ -0,0 +1,12 @@
+/**************************
+ * OBJETIVO: Criar os Cards de compra do Site
+ * 
+ * *************************/
+
+const produtos = ['produtos.json']
+
+const containerCard = document.getElementById('container-card');
+
+function criarCards(){
+
+}
