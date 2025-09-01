0a24258f84d25c6f8f27c26a3bc141d519ed4db7
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@dsn127m1249386.cfp127aluno.local>
2025-09-01 11:04:41 -0300

    criacao de objetos

--- app.js
+++ app.js
@@ -5,8 +5,31 @@
 
 const produtos = ['produtos.json']
 
-const containerCard = document.getElementById('container-card');
+const containerCard = document.getElementById('cards');
 
-function criarCards(){
+
+function criarCards(produtos){
+
+    
+
+    let cards = document.createElement('div');
+    cards.classList.add('cards');
+
+    let imagem = document.createElement('img');
+    imagem.src = `./img/${produtos.imagem}`;
+    imagem.classList.add('cards img');
+    
+    let nome = document.createElement('h3');
+    nome.textContent = produtos.nome;
+
+    
+    let descricao = document.createElement('p');
+    descricao.textContent = produtos.descricao;
+
+    let preco = document.createElement('h2');
+    preco.textContent = produtos.preco;
+    preco.classList.add('informacoes h2');
 
 }
+
+Cards.appendChild(cards);
